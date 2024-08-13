bf2a480d35b7f047bc69370250cc8a3507832117
mizuki.yamada <mizuki.yamada@e-seikatsu.co.jp>
2024-08-13 19:42:51 +0900

    スキーマを定義

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const {ApolloServer, gql} = require("apollo-server")
+
+// ref https://www.apollographql.com/docs/apollo-server/v2
+
+// define schema
+const typeDefs = gql`
+	type Query {
+		info: String!
+	}
+`
