e0224c9779672302c1f8f39f8297dd0b2425187b
mizuki.yamada <yamada.mizuki.0730@gmail.com>
2024-08-18 15:27:08 +0900

    tokenをjwtで暗号化

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,5 +1,9 @@
 // resolver for add user Mutation
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken")
+
+// 署名するための鍵、サーバ側のみが持つ。第3者がtokenを拾ってもこのAPP_SECRETを知らないと複合はできない。
+const APP_SECRET = "Graphql"
 
 // ここでのargsはschema.graphqlで設定したsignupが受け取る3つの引数
 async function singup(parent, args, context) {
@@ -14,4 +18,7 @@ async function singup(parent, args, context) {
 			password
 		}
 	})
+	const token = jwt.sign({ userId: user.id }, APP_SECRET)
+	// schema.graphqlで定義した戻り値の型を合うデータを返す
+	return {token,user}
 }
